941d686b76a7dcbebb948ca758016705277798a0
O.Ovchynnikov <oleksiiovchynnikovdev@gmail.com>
2024-04-08 12:39:06 +0200

    build fix

--- Assets/Infrastructure/Extensions/EditorExtensions/ConfigUpdater.cs
+++ Assets/Infrastructure/Extensions/EditorExtensions/ConfigUpdater.cs
@@ -1,12 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
 using NorskaLib.Spreadsheets;
 using UnityEditor;
 using UnityEngine;
-using Object = System.Object;
-using SpreadsheetImporter = NorskaLib.Spreadsheets.SpreadsheetImporter;
 
 namespace Infrastructure.Extensions.EditorExtensions
 {
